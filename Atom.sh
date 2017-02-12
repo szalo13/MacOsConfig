213b261ce961c340d2216b3f1816ca99d6d00063
szalo13 <kamil_sza@yahoo.pl>
2017-02-12 12:57:25 +0100

    Adding Atom plugins

---
+++ Atom.sh
@@ -0,0 +1,19 @@
+apm install multi-cursor
+
+apm install emmet
+
+apm install minimap
+
+apm install atom-beautify
+
+apm install linter
+
+apm install git-plus
+
+apm install file-icons
+
+apm install atom-javascript-snippets
+
+apm install csscomb-atom
+
+apm install color-picker
