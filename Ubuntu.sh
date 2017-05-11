963b23ec39853dc117ee18fe0a3eb1ba07a06a42
szalo13 <kamil_sza@yahoo.pl>
2017-05-11 14:24:58 +0200

    Adding Ubuntu configuration file

---
+++ Ubuntu.sh
@@ -0,0 +1,13 @@
+sudo -i
+
+# BASH
+
+  # Process viewer
+  apt-get install htop
+
+  # Process viewer / loads information
+  apt-get install atop
+
+  # Gnome-open - open files from terminal
+  apt-get install libgnome2-bin
+  
